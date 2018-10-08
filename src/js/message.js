2581fa7f1dc514f5641910088904440b3ed218c2
b0ses <b0ses@users.noreply.github.com>
2018-10-08 16:36:49 -0400

    Cleaning up react

---
+++ src/js/message.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Message extends Component {
+  generateClasses() {
+    const { visibility } = this.props;
+    const { effect } = this.props;
+    const classes = [visibility];
+    if (effect) {
+      classes.push(effect);
+    }
+    return classes.join(' ');
+  }
+
+  render() {
+    const { classes } = this.generateClasses();
+    const { content } = this.props;
+    return (
+      <p id="text" className={classes} style={{ textAlign: 'center' }}>
+        { content }
+      </p>
+    );
+  }
+}
+
+Message.propTypes = {
+  content: PropTypes.string,
+  visibility: PropTypes.string,
+  effect: PropTypes.string
+};
+
+Message.defaultProps = {
+  content: '',
+  visibility: 'hidden',
+  effect: ''
+};
+
+export default Message;
